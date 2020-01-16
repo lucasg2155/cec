9fea31ec0f8ea22aaf061fb04abe96e21e2d51d6
lucasg2155 <ggcon.gabriel@gmail.com>
2020-01-16 14:03:48 -0300

    cec 3 commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,7 @@ module.exports = {
   plugins: [`gatsby-plugin-netlify-cms`],
   plugins: [
     `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/blog`,
-        name: 'markdown-pages',
-      },
-    },
+   
     `gatsby-transformer-remark`,
   ],
 
